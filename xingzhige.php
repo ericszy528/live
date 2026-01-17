fa475cfa14919be61b283a34688165808b641c5e
ericszy528 <65778084+ericszy528@users.noreply.github.com>
2026-01-17 20:58:10 +0800

    Update and rename 18.json to xingzhige.php

---
+++ xingzhige.php
@@ -0,0 +1,269 @@
+{
+    "list": [
+        {
+            "vod_name": "乱世送亲令：我被分配了女帝老婆",
+            "vod_pic": "https://p3-reading-sign.fqnovelpic.com/novel-pic/35991057888cd341e2dcb28eaab8c087~tplv-snk2bdmkp8-super-reso-double-5-v2:600:0.image?lk3s=ae45642e&x-expires=1773838104&x-signature=FpggefT%2B30NHIbkaLu9vEj2KcGo%3D",
+            "vod_remarks": "古风权谋·穿越·全78集",
+            "vod_id": "NzU4MDI2NjM5MjcxNzM4MDYzMiwj5Lmx5LiW6YCB5Lqy5Luk77ya5oiR6KKr5YiG6YWN5LqG5aWz5bid6ICB5amG"
+        },
+        {
+            "vod_name": "乱世边军，开局一馒头救亡国女帝",
+            "vod_pic": "https://p3-reading-sign.fqnovelpic.com/novel-pic/3eaf7febbf526554552b6e21498b8737~tplv-snk2bdmkp8-super-reso-double-5-v2:600:0.image?lk3s=ae45642e&x-expires=1773838104&x-signature=XJFPliumLxOshrvFy6Gi2Gqnq4Y%3D",
+            "vod_remarks": "古风权谋·穿越·全80集",
+            "vod_id": "NzU4NzcxNDM4MDE5Njc2MDYwMSwj5Lmx5LiW6L655Yab77yM5byA5bGA5LiA6aaS5aS05pWR5Lqh5Zu95aWz5bid"
+        },
+        {
+            "vod_name": "末世女帝养成指南",
+            "vod_pic": "https://p3-reading-sign.fqnovelpic.com/novel-pic/7af0b76e4164342c9d3f1c7c7edb4b1b~tplv-snk2bdmkp8-super-reso-double-5-v2:600:0.image?lk3s=ae45642e&x-expires=1773838104&x-signature=pb%2BFKranZ5s4WPedSmRNNjXCM5g%3D",
+            "vod_remarks": "女性成长·穿越·全72集",
+            "vod_id": "NzU5MTQ4MTY2NjcwMzQ3Nzc4NSwj5pyr5LiW5aWz5bid5YW75oiQ5oyH5Y2X"
+        },
+        {
+            "vod_name": "开局女帝盯上了我的彩礼",
+            "vod_pic": "https://p3-reading-sign.fqnovelpic.com/novel-pic/3fdf3b96e4196d60e718622258d57674~tplv-snk2bdmkp8-super-reso-double-5-v2:600:0.image?lk3s=ae45642e&x-expires=1773838104&x-signature=%2Fka6mTLlreZdB%2FPv7tn1%2BML0iqc%3D",
+            "vod_remarks": "奇幻脑洞·穿越·全78集",
+            "vod_id": "NzQxMjgzOTA5MDEwNzczMDk2OSwj5byA5bGA5aWz5bid55uv5LiK5LqG5oiR55qE5b2p56S8"
+        },
+        {
+            "vod_name": "穿书反派女帝跟班，我被偷听心声",
+            "vod_pic": "https://p3-reading-sign.fqnovelpic.com/novel-pic/bb9f90591af38c2fc791f0389b812116~tplv-snk2bdmkp8-super-reso-double-5-v2:600:0.image?lk3s=ae45642e&x-expires=1773838104&x-signature=8LriFe3DIPEBXz1hGxOcxcS0piA%3D",
+            "vod_remarks": "奇幻脑洞·穿书·全80集",
+            "vod_id": "NzU5MTg1Mjc1MjAxNjI2NDIxNiwj56m/5Lmm5YabcdN5rSabcd5aWz5bid6Lef54abcdt77yM5oiR6KKr5YG35ZCs5babcdD5aOw"
+        },
+        {
+            "vod_name": "外卖小哥穿越护女帝定江山",
+            "vod_pic": "https://p3-reading-sign.fqnovelpic.com/novel-pic/b1cd03c1062b2f7ec8c8de737ee33d47~tplv-snk2bdmkp8-super-reso-double-5-v2:600:0.image?lk3s=ae45642e&x-expires=1773838104&x-signature=p91RI2t1qbmxrKR5kNFzRnVEfII%3D",
+            "vod_remarks": "奇幻爱情·穿越·全100集",
+            "vod_id": "NzU5NTA3ODg3MjY2NDY1NjkyMCwj5aSW5Y2W5bCP5ZOl56m/6LaK5oqk5aWz5bid5a6a5rGf5bGx"
+        },
+        {
+            "vod_name": "女帝，该还债了",
+            "vod_pic": "https://p3-reading-sign.fqnovelpic.com/novel-pic/4e6af94283c094c5b552702378161b61~tplv-snk2bdmkp8-super-reso-double-5-v2:600:0.image?lk3s=ae45642e&x-expires=1773838104&x-signature=TA8Y45a7hsupvXh%2BChKP%2Fv3Y4pg%3D",
+            "vod_remarks": "仙侠情缘·穿越·全71集",
+            "vod_id": "NzU3NDM2ODk2OTkyMjk3MjY5Nywj5aWz5bid77yM6Kabcdl6LabcdY5YC65LqG"
+        },
+        {
+            "vod_name": "女帝莫慌，臣有亿点点计谋",
+            "vod_pic": "https://p3-reading-sign.fqnovelpic.com/novel-pic/da02a771e5f20e7eeda7d25576802c55~tplv-snk2bdmkp8-super-reso-double-5-v2:600:0.image?lk3s=ae45642e&x-expires=1773838104&x-signature=Z5M%2B59pOXZdNYIjnefqmsDpiH9g%3D",
+            "vod_remarks": "古风权谋·穿越·全76集",
+            "vod_id": "NzU1NjkwMTM3MDU5OTU5MDkzNywj5aWz5bid6I6r5oWM77yM6Iej5pyJ5Lq/54K554K56K6h6LCL"
+        },
+        {
+            "vod_name": "女帝作死后，战神跑路了",
+            "vod_pic": "https://p3-reading-sign.fqnovelpic.com/novel-pic/fae0db13c48fc96d12f08fb5b29c2358~tplv-snk2bdmkp8-super-reso-double-5-v2:600:0.image?lk3s=ae45642e&x-expires=1773838104&x-signature=692I2RW9w3QrV%2FmtW1guEtFAkE8%3D",
+            "vod_remarks": "历史古代·打脸虐渣·全72集",
+            "vod_id": "NzU4MzAwMDMwODQ4NjI2MTc4NCwj5aWz5bid5L2c5q275ZCO77yM5oiY56We6LeR6Lev5LqG"
+        },
+        {
+            "vod_name": "手段太黑，女帝嫌我路子野",
+            "vod_pic": "https://p3-reading-sign.fqnovelpic.com/novel-pic/230ce8b6a99100cacc8103923e24572a~tplv-snk2bdmkp8-super-reso-double-5-v2:600:0.image?lk3s=ae45642e&x-expires=1773838104&x-signature=rtwy4q131%2BFsxZ58GW23J4bEc%2Bk%3D",
+            "vod_remarks": "古风权谋·穿越·全80集",
+            "vod_id": "NzU3NTA2MTE0OTY3OTM3MzM3NCwj5omL5q615aSq6buR77yM5aWz5bid5auM5oiR6Lev5a2Q6YeO"
+        }
+    ],
+    "filters": {},
+    "code": 1,
+    "msg": 200,
+    "class": [
+        {
+            "type_id": "总裁",
+            "type_name": "总裁"
+        },
+        {
+            "type_id": "女帝",
+            "type_name": "女帝"
+        },
+        {
+            "type_id": "都市",
+            "type_name": "都市"
+        },
+        {
+            "type_id": "赘婿",
+            "type_name": "赘婿"
+        },
+        {
+            "type_id": "战神",
+            "type_name": "战神"
+        },
+        {
+            "type_id": "古代言情",
+            "type_name": "古代言情"
+        },
+        {
+            "type_id": "现代言情",
+            "type_name": "现代言情"
+        },
+        {
+            "type_id": "历史",
+            "type_name": "历史"
+        },
+        {
+            "type_id": "脑洞",
+            "type_name": "脑洞"
+        },
+        {
+            "type_id": "玄幻",
+            "type_name": "玄幻"
+        },
+        {
+            "type_id": "电视节目",
+            "type_name": "电视节目"
+        },
+        {
+            "type_id": "搞笑",
+            "type_name": "搞笑"
+        },
+        {
+            "type_id": "网剧",
+            "type_name": "网剧"
+        },
+        {
+            "type_id": "喜剧",
+            "type_name": "喜剧"
+        },
+        {
+            "type_id": "萌宝",
+            "type_name": "萌宝"
+        },
+        {
+            "type_id": "神豪",
+            "type_name": "神豪"
+        },
+        {
+            "type_id": "致富",
+            "type_name": "致富"
+        },
+        {
+            "type_id": "奇幻脑洞",
+            "type_name": "奇幻脑洞"
+        },
+        {
+            "type_id": "超能",
+            "type_name": "超能"
+        },
+        {
+            "type_id": "强者回归",
+            "type_name": "强者回归"
+        },
+        {
+            "type_id": "甜宠",
+            "type_name": "甜宠"
+        },
+        {
+            "type_id": "励志",
+            "type_name": "励志"
+        },
+        {
+            "type_id": "豪门恩怨",
+            "type_name": "豪门恩怨"
+        },
+        {
+            "type_id": "复仇",
+            "type_name": "复仇"
+        },
+        {
+            "type_id": "长生",
+            "type_name": "长生"
+        },
+        {
+            "type_id": "神医",
+            "type_name": "神医"
+        },
+        {
+            "type_id": "马甲",
+            "type_name": "马甲"
+        },
+        {
+            "type_id": "亲情",
+            "type_name": "亲情"
+        },
+        {
+            "type_id": "小人物",
+            "type_name": "小人物"
+        },
+        {
+            "type_id": "奇幻",
+            "type_name": "奇幻"
+        },
+        {
+            "type_id": "无敌",
+            "type_name": "无敌"
+        },
+        {
+            "type_id": "现实",
+            "type_name": "现实"
+        },
+        {
+            "type_id": "重生",
+            "type_name": "重生"
+        },
+        {
+            "type_id": "闪婚",
+            "type_name": "闪婚"
+        },
+        {
+            "type_id": "职场商战",
+            "type_name": "职场商战"
+        },
+        {
+            "type_id": "穿越",
+            "type_name": "穿越"
+        },
+        {
+            "type_id": "年代",
+            "type_name": "年代"
+        },
+        {
+            "type_id": "权谋",
+            "type_name": "权谋"
+        },
+        {
+            "type_id": "高手下山",
+            "type_name": "高手下山"
+        },
+        {
+            "type_id": "悬疑",
+            "type_name": "悬疑"
+        },
+        {
+            "type_id": "家国情仇",
+            "type_name": "家国情仇"
+        },
+        {
+            "type_id": "虐恋",
+            "type_name": "虐恋"
+        },
+        {
+            "type_id": "古装",
+            "type_name": "古装"
+        },
+        {
+            "type_id": "时空之旅",
+            "type_name": "时空之旅"
+        },
+        {
+            "type_id": "玄幻仙侠",
+            "type_name": "玄幻仙侠"
+        },
+        {
+            "type_id": "欢喜冤家",
+            "type_name": "欢喜冤家"
+        },
+        {
+            "type_id": "传承觉醒",
+            "type_name": "传承觉醒"
+        },
+        {
+            "type_id": "情感",
+            "type_name": "情感"
+        },
+        {
+            "type_id": "逆袭",
+            "type_name": "逆袭"
+        },
+        {
+            "type_id": "家庭",
+            "type_name": "家庭"
+        }
+    ]
+}
